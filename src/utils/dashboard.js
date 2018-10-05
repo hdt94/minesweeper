6798d13c4af0d3473f49fcc781215c610582f6bd
Héctor Daniel Torres <hdt94@hotmail.com>
2018-10-05 11:06:54 -0500

    Dashboard utils update

--- src/utils/dashboard.js
+++ src/utils/dashboard.js
@@ -1,10 +1,45 @@
-// Not exported functions
+function reshapeArray(array, shape) {
+  const reshaped = [];
 
-const getAdjacents = (dashboard) => {
-  const { cols, grid, rows } = dashboard;
+  for (let n = 0; n < shape[0]; n++) {
+    reshaped.push(array.slice(n * shape[1], (n + 1) * shape[1]));
+  }
+
+  return reshaped;
+}
+
+function createBoxes(shape, grid, adjacents, mined) {
+  const boxes = {};
+
+  for (let r = 0; r < shape[0]; r++) {
+    for (let c = 0; c < shape[1]; c++) {
+      const id = grid[r][c];
+
+      boxes[id] = {
+        adjacents: adjacents[r][c],
+        adjacentMinesCount: 0,
+        id,
+        marked: false,
+        mined: mined[r][c],
+        visible: false,
+      };
+    }
+  }
+
+  Object.keys(boxes).forEach((id) => {
+    boxes[id].adjacentMinesCount = boxes[id].adjacents.reduce((
+      count,
+      adjacentId,
+    ) => count + Number(boxes[adjacentId].mined), 0);
+  });
+
+  return boxes;
+}
+
+function getAdjacents(shape, grid) {
   const adj = grid.map(row => row.map(() => []));
-  const clast = cols - 1;
-  const rlast = rows - 1;
+  const clast = shape[1] - 1;
+  const rlast = shape[0] - 1;
 
   for (let r = 0; r < rlast; r++) {
     for (let c = 0; c < clast; c++) {
@@ -22,52 +57,32 @@ const getAdjacents = (dashboard) => {
   }
 
   return adj;
-};
+}
 
+function getMined(random, random2d, grid, mines) {
+  random.sort((a, b) => b - a); // descending order
 
-// Exported functions
+  const bottomLim = random[mines];
 
-export const createBoxes = (dashboard) => {
-  const adj = getAdjacents(dashboard);
-  const boxes = {};
-  const { cols, grid, rows } = dashboard;
+  return grid.map((row, r) => row.map((_, c) => random2d[r][c] > bottomLim));
+}
 
-  for (let r = 0; r < rows; r++) {
-    for (let c = 0; c < cols; c++) {
-      const id = grid[r][c];
-
-      boxes[id] = {
-        adjacents: adj[r][c],
-        id,
-        state: 'blank',
-      };
-    }
-  }
-
-  return boxes;
-};
-
-export const createBoxIds = (length) => {
-  const arr = [];
+export default (length, rows, mines) => {
+  const boxIds = [];
+  const random = [];
+  const shape = [rows, length / rows];
 
   for (let n = 0; n < length; n++) {
-    arr.push(n);
+    boxIds.push(n);
+    random.push(Math.random());
   }
 
-  return arr;
-};
+  const grid = reshapeArray(boxIds, shape);
+  const random2d = reshapeArray(random, shape);
 
-export const createDashboard = (boxIds, rows) => {
-  const cols = boxIds.length / rows;
-  const dashboard = {
-    cols,
-    grid: [],
-    rows,
-  };
+  const adjacents = getAdjacents(shape, grid);
+  const mined = getMined(random, random2d, grid, mines);
+  const boxes = createBoxes(shape, grid, adjacents, mined);
 
-  for (let n = 0; n < rows; n++) {
-    dashboard.grid.push(boxIds.slice(n * cols, (n + 1) * cols));
-  }
-
-  return dashboard;
+  return { boxes, grid, length, shape };
 };
