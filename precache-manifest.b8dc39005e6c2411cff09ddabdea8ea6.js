023c3f23f522cc246d83d86a97d200991c1e392e
Héctor Daniel Torres <hdt94@hotmail.com>
2018-10-06 15:39:32 -0500

    Updates

---
+++ precache-manifest.b8dc39005e6c2411cff09ddabdea8ea6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e96a486204ff57e2541f",
+    "url": "/minesweeper/static/js/runtime~main.e96a4862.js"
+  },
+  {
+    "revision": "6ac74c3732041d8e96bf",
+    "url": "/minesweeper/static/js/main.6ac74c37.chunk.js"
+  },
+  {
+    "revision": "4f6c2726edc5ff17875d",
+    "url": "/minesweeper/static/js/1.4f6c2726.chunk.js"
+  },
+  {
+    "revision": "6ac74c3732041d8e96bf",
+    "url": "/minesweeper/static/css/main.815abec4.chunk.css"
+  },
+  {
+    "revision": "49a9c258279bf74a52bd9fd2a7d1376a",
+    "url": "/minesweeper/index.html"
+  }
+];
